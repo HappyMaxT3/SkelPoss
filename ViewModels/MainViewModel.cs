6a3f0171c369b98fb566eef8391f2864a52e42ed
HappyMaxT3 <m9775531181@gmail.com>
2025-02-19 18:11:22 +0300

    warning fixes

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -53,8 +53,8 @@ namespace SkelAppliences.ViewModels
         public string Text { get; set; } = string.Empty;
 
         public bool IsProduct { get; set; }
-        public string ProductName { get; set; } 
-        public string ProductDetails { get; set; } 
-        public string ProductImage { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDetails { get; set; } = string.Empty;
+        public string ProductImage { get; set; } = string.Empty;
     }
 }
