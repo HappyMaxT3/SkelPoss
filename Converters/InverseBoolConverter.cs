890d0c276c9ca52a73289beeb1c513aad4254014
HappyMaxT3 <m9775531181@gmail.com>
2025-02-22 13:51:32 +0300

    message delination

--- Converters/InverseBoolConverter.cs
+++ Converters/InverseBoolConverter.cs
@@ -7,21 +7,22 @@ namespace SkelAppliences.Converters
     public class InverseBoolConverter : IValueConverter
     {
         public static InverseBoolConverter Instance { get; } = new InverseBoolConverter();
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool boolValue)
             {
-                return !boolValue;
+                if (parameter?.ToString() == "End")
+                    return boolValue ? LayoutOptions.End : LayoutOptions.Start;
+                else
+                    return !boolValue;
             }
             return value;
         }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is bool boolValue)
-            {
-                return !boolValue;
-            }
-            return value;
+            throw new NotImplementedException();
         }
     }
 
