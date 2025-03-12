52983cd6b3dee70ed728dd5e13ad7d034528d88c
HappyMaxT3 <m9775531181@gmail.com>
2025-03-12 19:08:25 +0300

    fixed warnings

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -5,14 +5,19 @@ using Android.Views;
 
 namespace SkelAppliences
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                               ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#343434"));
+            if (Window != null)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#343434"));
+            }
         }
     }
 }
