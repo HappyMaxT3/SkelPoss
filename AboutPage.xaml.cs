071d92af9d36d1616c26443c8edf1447ffe1b363
HappyMaxT3 <m9775531181@gmail.com>
2025-03-21 21:02:30 +0300

    added developers page

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TechnoPoss;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+        BindingContext = new AboutViewModel();
+    }
+
+    private void OnSwiped(object sender, SwipedEventArgs e)
+    {
+        if (e.Direction == SwipeDirection.Right)
+        {
+            Shell.Current.FlyoutIsPresented = true;
+        }
+    }
+}
---
+++ ViewModels/AboutViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+
+public class AboutViewModel : ContentPage
+{
+    public ObservableCollection<Developer> Developers { get; } = new();
+
+    public AboutViewModel()
+    {
+        LoadDevelopers();
+    }
+
+    private void LoadDevelopers()
+    {
+        Developers.Add(new Developer
+        {
+            Photo = "lena_photo.jpg",
+            Name = "Лена Бибизян",
+            Role = "Designer"
+        });
+
+        Developers.Add(new Developer
+        {
+            Photo = "zabus_photo.jpg",
+            Name = "Алена Забус",
+            Role = "Designer"
+        });
+
+        Developers.Add(new Developer
+        {
+            Photo = "max_photo.jpg",
+            Name = "Макс Гослинг",
+            Role = "Team Lead / .Net MAUI Developer"
+        });
+
+        Developers.Add(new Developer
+        {
+            Photo = "denis_photo.jpg",
+            Name = "Денис Опосс",
+            Role = "Backend Developer"
+        });
+
+        Developers.Add(new Developer
+        {
+            Photo = "misha_photo.jpg",
+            Name = "Миша Лоск",
+            Role = "Backend Developer"
+        });
+    }
+}
+
+public class Developer
+{
+    public string Photo { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
