61a8c0d4617768e7b4da2025294bbd2f257193e1
HappyMaxT3 <m9775531181@gmail.com>
2025-03-22 14:52:25 +0300

    Update TechRadarParser.cs

--- Services/TechRadarParser.cs
+++ Services/TechRadarParser.cs
@@ -75,7 +75,7 @@ namespace TechnoPoss.Services
             return articles.Select(article => new NewsItem
             {
                 Source = "techradar.com",
-                SourceColor = "C71585",  
+                SourceColor = "#00A1D6",  
                 Title = ExtractTitle(article),
                 Content = ExtractContent(article),
                 Url = ExtractUrl(article),
